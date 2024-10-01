6a768097a8c61784bb7410a877fa6cf51fc69333
madscientist <fikunyin.adebola@gmail.com>
2024-10-01 07:14:46 +0100

    Vanilla Templates and Dataset Added

---
+++ Vanilla Frontend/js/script.js
@@ -0,0 +1,7 @@
+// JavaScript to handle gallery image clicks and redirect to planet pages
+function openPlanetPage(planetName) {
+    // For now, simply log the planet name, can redirect to individual pages
+    console.log("Redirecting to planet page: " + planetName);
+    // window.location.href = planetName + '.html'; // Uncomment to redirect
+  }
+  
